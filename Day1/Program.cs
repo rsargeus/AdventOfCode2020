10aa466367c2b3cdc5853fb3fb037efa703b2d9b
Robert Sargéus <robert.sargeus@geshdo.com>
2020-12-01 13:18:39 +0100

    Second part

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -11,9 +11,10 @@ namespace Day1
             string inputtext = System.IO.File.ReadAllText(@"input.txt");
             int[] values = inputtext.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s)).ToArray();
 
-            int part1 = GetPart1(values);
+            //int part1 = GetPart1(values);
+            int part2 = GetPart2(values);
 
-            Console.WriteLine(part1);
+            Console.WriteLine(part2);
         }
 
         private static int GetPart1(int[] ints)
@@ -27,6 +28,30 @@ namespace Day1
                     if (sum == 2020)
                     {
                         return (ints[i] * ints[j]);
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Could not find product equals 2020");
+        }
+
+        private static int GetPart2(int[] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {                
+                for (int j = ints.Length - 1; j >= 0; j--)
+                {
+                    for (int k = 0; k < ints.Length; k++)
+                    {
+                        if(ints[k] != ints[i] && ints[k] != ints[j])
+                        {
+                            int sum = ints[i] + ints[j] + ints[k];
+                            if (sum == 2020)
+                            {
+                                return (ints[i] * ints[j] * ints[k]);
+                            }
+                        }
+   
                     }
                 }
             }
