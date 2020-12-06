82733d41e8b580e3a6cc1a351d0903c3687ad9f5
Robert Sargéus <robert.sargeus@geshdo.com>
2020-12-06 09:00:37 +0100

    Day 5 Alterante solution

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Day5
@@ -11,26 +10,35 @@ namespace Day5
             string inputtext = System.IO.File.ReadAllText(@"input.txt");
             string[] values = inputtext.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
-            IEnumerable<int> seatIds = values.Select(boardingpass =>
+            var seatIds = values.Select(boardingpass =>
             {
-                (int, int, int, int) seat = (0, 127, 0, 7);
-                foreach (char c in boardingpass)
-                {
-                    seat = c switch
-                    {
-                        'F' => (seat.Item1, seat.Item1 + (seat.Item2 - seat.Item1) / 2, seat.Item3, seat.Item4),
-                        'B' => ((seat.Item2 - (seat.Item2 - seat.Item1) / 2), seat.Item2, seat.Item3, seat.Item4),
-                        'L' => (seat.Item1, seat.Item2, seat.Item3, seat.Item3 + (seat.Item4 - seat.Item3) / 2),
-                        'R' => (seat.Item1, seat.Item2, (seat.Item4 - (seat.Item4 - seat.Item3) / 2), seat.Item4)
-                    };
-                }
-                return seat.Item1 * 8 + seat.Item3;
+                string bytes = boardingpass
+                    .Replace('F', '0')
+                    .Replace('B', '1')
+                    .Replace('L', '0')
+                    .Replace('R', '1');
+
+                return Convert.ToInt32(bytes, 2);
+
+                //(int, int, int, int) seat = (0, 127, 0, 7);
+                //foreach (char c in boardingpass)
+                //{
+                //    seat = c switch
+                //    {
+                //        'F' => (seat.Item1, seat.Item1 + (seat.Item2 - seat.Item1) / 2, seat.Item3, seat.Item4),
+                //        'B' => ((seat.Item2 - (seat.Item2 - seat.Item1) / 2), seat.Item2, seat.Item3, seat.Item4),
+                //        'L' => (seat.Item1, seat.Item2, seat.Item3, seat.Item3 + (seat.Item4 - seat.Item3) / 2),
+                //        'R' => (seat.Item1, seat.Item2, (seat.Item4 - (seat.Item4 - seat.Item3) / 2), seat.Item4)
+                //    };
+                //}
+                //return (boardingpass, seat.Item1 * 8 + seat.Item3);
             });
 
             int part1 = seatIds.Max();
             int part2 = Enumerable.Range(seatIds.Min(), seatIds.Count()).Except(seatIds).Single();
 
-            Console.WriteLine(part2);
+            Console.WriteLine($"Part 1: {part1}, Part 2: {part2}");
+            Console.ReadLine();
         }
     }
 }
